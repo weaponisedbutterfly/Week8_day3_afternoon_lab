b63fc78555f2bae7299c78748dd3d51c332e21ac
Nick Riddell <nickjriddell@gmail.com>
2016-01-27 15:20:43 +0000

    his health goes up by a factor of 1.5 when eating his fav food

--- hero_game.js
+++ hero_game.js
@@ -1,14 +1,25 @@
+var food = require('./hero_game_food');
 var Hero = function(name, health, favFood){
   this.name = name;
   this.health = health;
   this.favFood = favFood;
   this.talk = function(){
     console.log('My name is ' + this.name);
+  },
+  this.eat = function(){
+    console.log('I fancy a ' + food.name + '. As my health is' + this.health);
+    if(food.name === favFood){
+      this.health += food.repValue * 1.5;
+    }else{
+      this.health += food.repValue;
+    }
+    console.log('That ' + food.name + ' was very satisfying. My health is now ' + this.health);
   }
 }
 
 var hero1 = new Hero('Corban', 100, 'banana');
 
 hero1.talk();
+hero1.eat();
 
 module.exports = hero1;
--- hero_game_spec.js
+++ hero_game_spec.js
@@ -7,6 +7,12 @@ describe('Hero', function(){
   it('name should be Corban', function(){
     assert.equal('Corban', hero.name);
   });
+
+  it('Corbans health should increase by 3 to 78', function(){
+  hero.health = 75;
+  hero.eat();
+  assert.equal(78, hero.health);
+  });
 });
 
 describe('Food', function(){
