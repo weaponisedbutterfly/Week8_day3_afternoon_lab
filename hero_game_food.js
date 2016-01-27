db17c1060d81e7c8190d6f3e1dbdda1f84eb5be5
Nick Riddell <nickjriddell@gmail.com>
2016-01-27 14:53:29 +0000

    a banana is indeed a banana

---
+++ hero_game_food.js
@@ -0,0 +1,11 @@
+var Food = function(name, repValue){
+  this.name = name;
+  this.repValue = repValue;
+}
+
+var food1 = new Food('banana', 2);
+var food2 = new Food('dairylea', 1);
+var food3 = new Food('egg', 1);
+var food4 = new Food('roastBeef', 5);
+
+module.exports = food1;
--- hero_game_spec.js
+++ hero_game_spec.js
@@ -1,4 +1,5 @@
 var hero = require('./hero_game');
+var food = require('./hero_game_food');
 var assert = require('assert');
 
 
@@ -6,4 +7,11 @@ describe('Hero', function(){
   it('name should be Corban', function(){
     assert.equal('Corban', hero.name);
   });
+});
+
+describe('Food', function(){
+  it('name should be banana', function(){
+    assert.equal('banana', food.name);
   });
+});
+
